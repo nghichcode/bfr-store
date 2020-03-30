f0e29b3b35ae2b297409e8a4cf9efe5308903dfb
nghichcode <nghichcode@gmail.com>
2020-03-30 23:26:26 +0700

    Syage 0.1 README.md

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import 'react-native-gesture-handler';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import React from 'react';
 import {
@@ -10,7 +10,7 @@ import {
   Text,
   // Button
 } from 'react-native';
-import { Button, SearchBar } from 'react-native-elements';
+import {Button, SearchBar} from 'react-native-elements';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 // import Icon from 'react-native-vector-icons/FontAwesome';
 
@@ -18,186 +18,201 @@ const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 class Home extends React.Component {
-	constructor(props) {
+  constructor(props) {
     super(props);
-		this.state = {
-			search: '',
-		};
-	}
-	updateSearch = search => {
-    this.setState({ search });
+    this.state = {
+      search: '',
+    };
+  }
+
+  updateSearch = search => {
+    this.setState({search});
   };
 
-	render() {
-		const { search } = this.state;
-		const {navigation} = this.props;
-	  return (
-	  	<View>
-	  		<View style={{flex: 1, backgroundColor:'#CCC', flexDirection: "row", flexWrap:'wrap', paddingHorizontal:'2%', paddingTop: 2}}>
-		  		<View style={{flex: 10,}}>
-			  		<SearchBar
-			  			lightTheme={true}
-			        placeholder="Type Here..."
-			        onChangeText={this.updateSearch}
-			        value={search}
-			      />
-	      	</View>
-	      	<View style={{flex: 2, backgroundColor: '#FCF'}}>
-			    	<Text style={{
-			    		borderTopWidth: 1,
-					    borderBottomWidth: 1,
-					    borderBottomColor: '#000',
-					    borderTopColor: '#000',
-					    padding: 8,
-					    backgroundColor: '#CBC',
+  render() {
+    const {search} = this.state;
+    const {navigation} = this.props;
+    return (
+      <View>
+        <View style={{
+          flex: 1,
+          backgroundColor: '#CCC',
+          flexDirection: "row",
+          flexWrap: 'wrap',
+          paddingHorizontal: '2%',
+          paddingTop: 2
+        }}>
+          <View style={{flex: 10,}}>
+            <SearchBar
+              lightTheme={true}
+              placeholder="Type Here..."
+              onChangeText={this.updateSearch}
+              value={search}
+            />
+          </View>
+          <View style={{flex: 2, backgroundColor: '#FCF'}}>
+            <Text style={{
+              borderTopWidth: 1,
+              borderBottomWidth: 1,
+              borderBottomColor: '#000',
+              borderTopColor: '#000',
+              padding: 8,
+              backgroundColor: '#CBC',
 
-					    borderBottomWidth: 0,
-					    borderRadius: 3,
-					    overflow: 'hidden',
-					    minHeight: 30,
+              borderBottomWidth: 0,
+              borderRadius: 3,
+              overflow: 'hidden',
+              minHeight: 30,
 
-			    	}}
-			    	><Ionicons name='ios-barcode' size={30} color='#000' /></Text>
-	      	</View>
-	      </View>
-	      <View>
-			    <Button
-			      title="Go to Jane's profile"
-			      onPress={() => navigation.navigate('Profile', {name: 'Jane'})}
-			      containerStyle={{marginHorizontal:'10%', marginBottom:2, width:'80%', backgroundColor: 'red'}}
-			    />
-			    <Button
-			      title="Nest"
-			      onPress={() => navigation.navigate('Nest', {name: 'Jane'})}
-			      style={{margin:2, width:'80%'}}
-			    />
-			    <View style={{margin:2}}>
-			    	<Text>
-			    		<Ionicons name='ios-football' size={20} color='#CFC' />
-			    	</Text>
-			    	<Text>
-			    		{this.state.search}
-			    	</Text>		    	
-			    </View>
-		    </View>
-	    </View>
-	  );
-	}
+            }}
+            ><Ionicons name='ios-barcode' size={30} color='#000'/></Text>
+          </View>
+        </View>
+        <View>
+          <Button
+            title="Go to Jane's profile"
+            onPress={() => navigation.navigate('Profile', {name: 'Jane'})}
+            containerStyle={{
+              marginHorizontal: '10%',
+              marginBottom: 2,
+              width: '80%',
+              backgroundColor: 'red'
+            }}
+          />
+          <Button
+            title="Nest"
+            onPress={() => navigation.navigate('Nest', {name: 'Jane'})}
+            style={{margin: 2, width: '80%'}}
+          />
+          <View style={{margin: 2}}>
+            <Text>
+              <Ionicons name='ios-football' size={20} color='#CFC'/>
+            </Text>
+            <Text>
+              {this.state.search}
+            </Text>
+          </View>
+        </View>
+      </View>
+    );
+  }
 }
 
 class Profile extends React.Component {
-	render() {
-		const {navigation} = this.props;
-	  return (
-	    <Button
-	      title="Go to Jane's home"
-	      onPress={() => navigation.navigate('Nest', {name: 'Jane'})}
-	    />
-	  );
-	}
+  render() {
+    const {navigation} = this.props;
+    return (
+      <Button
+        title="Go to Jane's home"
+        onPress={() => navigation.navigate('Nest', {name: 'Jane'})}
+      />
+    );
+  }
 }
 
 class Feed extends React.Component {
-	render() {
-		const {navigation} = this.props;
-	  return (
-	  	<View>
-	  		<Text>Feed</Text>
-	  	</View>
-	  );
-	}
+  render() {
+    const {navigation} = this.props;
+    return (
+      <View>
+        <Text>Feed</Text>
+      </View>
+    );
+  }
 }
+
 class Messages extends React.Component {
-	render() {
-		const {navigation} = this.props;
-	  return (
-	  	<View>
-	  		<Text>Messages</Text>
-	  	</View>
-	  );
-	}
+  render() {
+    const {navigation} = this.props;
+    return (
+      <View>
+        <Text>Messages</Text>
+      </View>
+    );
+  }
 }
 
 
 function Nest() {
   return (
     <Tab.Navigator
-      screenOptions={({ route }) => ({
-	      tabBarIcon: ({ focused, color, size }) => {
-	        let iconName;
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
 
-	        if (route.name === 'Feed') {
-	          iconName = focused
-	            ? 'ios-information-circle'
-	            : 'ios-information-circle-outline';
-	        } else if (route.name === 'Messages') {
-	          iconName = focused ? 'ios-list-box' : 'ios-list';
-	        }
-	        return <Ionicons name={iconName} size={size} color={color} />;
-	      },
-	    })}
+          if (route.name === 'Feed') {
+            iconName = focused
+              ? 'ios-information-circle'
+              : 'ios-information-circle-outline';
+          } else if (route.name === 'Messages') {
+            iconName = focused ? 'ios-list-box' : 'ios-list';
+          }
+          return <Ionicons name={iconName} size={size} color={color}/>;
+        },
+      })}
 
       tabBarOptions={{
         activeTintColor: 'tomato',
         inactiveTintColor: 'gray',
       }}
     >
-      <Tab.Screen name="Feed" component={Feed} />
-      <Tab.Screen name="Messages" component={Messages} />
+      <Tab.Screen name="Feed" component={Feed}/>
+      <Tab.Screen name="Messages" component={Messages}/>
     </Tab.Navigator>
   );
 }
 
 class App extends React.Component {
-	render() {
-	  return (
-	    <NavigationContainer>
-	      <Stack.Navigator>
-	        <Stack.Screen
-	          name="Home"
-	          component={Home}
-	          options={{
-	          	title: 'Welcome', headerShown: true,
-	          	header: ({ scene, previous, navigation }) => {
-							  return (
-						  		<View style={{backgroundColor:'#CCC'}}>
-							  		<View style={{width: '90%'}}>
-								  		<SearchBar
-								  			lightTheme={true}
-								        placeholder="Type Here..."
-								      />
-						      	</View>
-						      	<View style={{
-						      		position: 'absolute',
-						      		right: 0,top: 0,width: '10%',height: '100%',
-						      		backgroundColor: '#e6e8ec'}}>
-								    	<Text style={{marginVertical: 12}}
-								    	><Ionicons name='ios-barcode' size={30} color='#000' /></Text>
-						      	</View>
-						      </View>
-							  );
-							},
-	        	}}
-	        />
-	        <Stack.Screen name="Profile" component={Profile}
-	        	options={{
-	        		title: 'Welcome', headerStyle: {backgroundColor: '#ff5722'},
-	        		headerTitleStyle: {color: '#fcfcfc'},
-	        		headerTintColor: '#fcfcfc',
-	        		headerRight: () => (
-		            <Button
-		              onPress={() => alert('This is a button!')}
-		              title="Info"
-		              color="#fff"
-		            />
-		          ),
-	        	}}
-	        />
-	        <Stack.Screen name="Nest" component={Nest} />
-	      </Stack.Navigator>
-	    </NavigationContainer>
-	  );
-	}
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{
+              title: 'Welcome', headerShown: true,
+              header: ({scene, previous, navigation}) => {
+                return (
+                  <View style={{backgroundColor: '#CCC'}}>
+                    <View style={{width: '90%'}}>
+                      <SearchBar
+                        lightTheme={true}
+                        placeholder="Type Here..."
+                      />
+                    </View>
+                    <View style={{
+                      position: 'absolute',
+                      right: 0, top: 0, width: '10%', height: '100%',
+                      backgroundColor: '#e6e8ec'
+                    }}>
+                      <Text style={{marginVertical: 12}}
+                      ><Ionicons name='ios-barcode' size={30} color='#000'/></Text>
+                    </View>
+                  </View>
+                );
+              },
+            }}
+          />
+          <Stack.Screen name="Profile" component={Profile}
+                        options={{
+                          title: 'Welcome', headerStyle: {backgroundColor: '#ff5722'},
+                          headerTitleStyle: {color: '#fcfcfc'},
+                          headerTintColor: '#fcfcfc',
+                          headerRight: () => (
+                            <Button
+                              onPress={() => alert('This is a button!')}
+                              title="Info"
+                              color="#fff"
+                            />
+                          ),
+                        }}
+          />
+          <Stack.Screen name="Nest" component={Nest}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
 
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# bfr-store
-BFR store
+# BFR Store
+
+###TÀI LIỆU MÔN PHÁT TRIỂN ỨNG DỤNG DI ĐỘNG
+>Lớp: INT3120 3  
+>Giảng viên: Nguyễn Việt Tân
+>  
+> Các thành viên:
+>* Nguyễn Văn Diên  
+>* Lê Duy Hưng  
+>* Ninh Thị Nhật Lệ
+>
+>##### Đề tài: Ứng dụng tra cứu thông tin hàng hoá
+>**Công nghệ: React Native, PHP (CodeIgniter)**
+
+##### Các chức năng:
+ 
+
+|STT| Tên chức năng | Mô tả |
+|---|---------------|-------|
+| 1 | Đăng nhập | Người dùng có thể đăng nhập hoặc sử dụng tài khoản guest để vào ứng dụng. |
+| 2 | Đăng ký   | Đăng ký tài khoản người dùng đối với cửa hàng hoặc admin. |
+| 3 | Phân quyền | Admin/Khách hàng/Guest |
+| 4 | Các thông tin được phép hiển thị đối với chủ cửa hàng | Cửa hàng có quyền hiển thị/ẩn các thông tin như: số tiền, số lượng hàng hóa... |
+| 5 | Trang tìm kiếm (mọi user) | Mọi user có quyền tìm kiếm và thêm thông tin hàng hóa. Khi tìm kiếm thành công, sẽ hiển thị  thông tin chung cũng như thông tin riêng đối với từng cửa hàng đã public thông tin. |
+| 6 | Trang cửa hàng (dành cho chủ cửa hàng) | Chủ cửa hàng có quyền tìm kiếm, xem, sửa (thông tin, số lượng hàng), xóa hàng hóa có trong cửa hàng. |
+| 7 | Trang tính toán | Cho phép người dùng tính toán tổng số tiền dựa vào các hàng hóa và số lượng hàng. |
+| 9 | Kiểm duyệt thông tin hàng hóa (Admin) | Admin có quyền kiểm duyệt thông tin hàng hóa |
+| 10 | Thông tin tài khoản | Dùng để xem thông tin tài khoản hoặc "Đăng xuất" khỏi hệ thống. |
